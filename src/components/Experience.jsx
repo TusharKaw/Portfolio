f4017f12c827de25440f9074e62ae2ba4b510da7
Tushar Kaw <108132018+TusharKaw@users.noreply.github.com>
2025-08-01 10:42:12 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,21 +5,36 @@ const Experience = () => {
   const experiences = [
     {
       id: 1,
+      position: 'Operations Manager',
+      company: 'NeVA - National Informatics Centre',
+      location: 'Delhi, India',
+      period: <text style={{color:'#000'}}>July 2025 - Present</text>,
+      responsibilities: [
+        'Leading the operational planning & execution of the NeVA (National e-Vidhan Application) project at the Delhi Legislative Assembly, for the digitization of legislative processes in alignment with the Government of India’s Digital India mission.',
+        'Coordinating with cross-functional teams including IT developers, legislative staff, and vendor partners to ensure seamless implementation of the e-Vidhan platform.',
+        'Facilitating communication between government stakeholders and technical teams, translating complex legislative workflows into user-centric digital interfaces.',
+        'Supervised quality assurance and compliance checks to ensure adherence to NIC and MeitY standards for security, accessibility, and data integrity.',
+        'Streamlined legislative operations by implementing paperless workflows and real-time document sharing, contributing to a 40% reduction in session preparation time.',
+        'Monitoring performance metrics and user feedback to iterate on workflows and platform features, leading to a 90% satisfaction rate among end-users.'
+      ]
+    },
+    {
+      id: 2,
       position: 'Project Manager',
       company: 'Abhiwan Technology Pvt Ltd',
       location: 'Noida, India',
-      period: <text style={{color:'#000'}}>March 2024 - Present</text>,
+      period: <text style={{color:'#000'}}>March 2024 - July 2025</text>,
       responsibilities: [
         'Leading and managing teams of developers and designers to successfully deliver 10+ projects, including web apps, games, VR apps, and blockchain-based projects.',
         'Overseeing project lifecycles from planning to deployment, ensuring on-time and on-budget execution.',
         'Communicating technical requirements effectively with stakeholders and leading the creation of detailed API documentation and database schema designs, contributing to a 95% client satisfaction rate.',
-        'ontributing to increasing user engagement by 30% on Cardano blockchain based apps and NFT based games.',
+        'Contributing to increasing user engagement by 30% on Cardano blockchain based apps and NFT based games.',
         'Collaborated with cross-functional teams to lead project execution, manage timelines, and ensure high-quality deliverables.',
         'Delivering comprehensive client solutions across Risk Management and QA domains, aligning scope, resources, and timelines with Agile/Scrum methodologies.'
       ]
     },
     {
-      id: 2,
+      id: 3,
       position: 'Project Manager Intern',
       company: 'Bharti Airtel',
       location: 'Gurugram, Haryana',
@@ -32,7 +47,7 @@ const Experience = () => {
       ]
     },
     {
-      id: 3,
+      id: 4,
       position: 'Network Engineer',
       company: 'Excitel Broadbands',
       location: 'Delhi, India',
